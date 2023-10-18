247dbc6dbdbfe12418aa5ace1f70e3717cfd0eef
Soma Sekine <a8121151@aoyama.jp>
2023-10-18 17:33:25 -0400

    ゲームオーバー処理実装、ただ三角形じゃないとゲームオーバー判定してくれない？

---
+++ Assets/MainScene/MaxBar.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MaxBar : MonoBehaviour
+{
+    private float touchDuration = 0f;
+    private bool isTouching = false;
+    private bool isGameOver = false;
+    public Canvas canvasGameover = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (canvasGameover != null)
+        {
+            canvasGameover.enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouching)
+        {
+            touchDuration += Time.deltaTime;
+            if (touchDuration >= 2f && !isGameOver) // 1秒以上接触していたら
+            {
+                // ゲームオーバーモーダルを表示する処理を呼び出す
+                ShowGameOverModal();
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        isTouching = true;
+        Debug.Log("Enter");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        isTouching = false;
+        touchDuration = 0f; // タッチ時間をリセット
+        
+    }
+
+    private void ShowGameOverModal()
+    {
+        isGameOver = true;
+        Debug.Log("Game Over");
+        if (canvasGameover != null)
+        {
+            canvasGameover.enabled = true;
+        }
+    }
+
+    public void onButtonRetry()
+    {
+        // Canvas を無効にする。(ダイアログを閉じる)
+        canvasGameover.enabled = false;
+        SceneManager.LoadScene("MainScene");
+        
+    }
+
+    public void onButtonBack()
+    {
+        // Canvas を無効にする。(ダイアログを閉じる)
+        canvasGameover.enabled = false;
+        SceneManager.LoadScene("MainScene");
+    }
+
+}
